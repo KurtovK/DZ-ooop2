f6b350bca74158acacb1149c4496950f2255922b
KKD <kossstyan87@mail.ru>
2023-03-30 12:24:07 +0300

    Добавил class car

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 #Реализуйте класс «Автомобиль». Необходимо хранить в полях класса:
 #название модели, год выпуска, производителя, объем двигателя, цвет машины,
 #цену. Реализуйте конструктор по умолчанию и метод для вывода данных.
-
+class Car:
+    def __init__(self, model='', year=0, manufacturer='', engine_volume=0, color='', price=0):
+        self.model = model
+        self.year = year
+        self.manufacturer = manufacturer
+        self.engine_volume = engine_volume
+        self.color = color
+        self.price = price
 
 
 
