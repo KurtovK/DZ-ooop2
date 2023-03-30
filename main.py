12212c81bd246485c4799f05c18da6c4e70ee4b2
KKD <kossstyan87@mail.ru>
2023-03-30 12:39:21 +0300

    Добавил class Stadium

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-#Задание 1.
-#Реализуйте класс «Автомобиль». Необходимо хранить в полях класса:
-#название модели, год выпуска, производителя, объем двигателя, цвет машины,
-#цену. Реализуйте конструктор по умолчанию и метод для вывода данных.
-class Car:
-    def __init__(self, model='', year=0, manufacturer='', engine_volume=0, color='', price=0):
-        self.model = model
-        self.year = year
-        self.manufacturer = manufacturer
-        self.engine_volume = engine_volume
-        self.color = color
-        self.price = price
+#Задание 2.
+#Реализуйте класс «Стадион». Необходимо хранить в полях класса:
+#название стадиона, дату открытия, страну, город, вместимость. Реализуйте
+#конструктор по умолчанию и метод для вывода данных.
+class Stadium:
+    def __init__(self):
+        self.name = ""
+        self.opening_date = ""
+        self.country = ""
+        self.city = ""
+        self.capacity = 0
 
     def display_info(self):
         print('Модель:', self.model)
@@ -22,7 +21,7 @@ class Car:
 
 
 def execute_application():
-    # Задание 1.
+    # Задание 2.
     car1 = Car('Sorento', 2021, 'KIA', 2.5, 'красный', 4079900)
     car1.display_info()
 
